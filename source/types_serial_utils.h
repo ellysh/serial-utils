4940233278d7305b557bb4726f8809a68c8fecd4
Ilya Shpigor <petrsum@gmail.com>
2013-03-11 15:44:00 +0400

    Add ProgramOptions class Add base implementation of the write-data utility

---
+++ source/types_serial_utils.h
@@ -0,0 +1,16 @@
+#ifndef TYPES_SERIAL_UTILS_H
+#define TYPES_SERIAL_UTILS_H
+
+namespace serial_utils
+{
+
+/* A macro to disallow the copy constructor and operator= functions.
+ * This should be used in the private: declarations for a class. */
+#define DISALLOW_COPY_AND_ASSIGN(TypeName) \
+private: \
+    TypeName(const TypeName&);               \
+    TypeName& operator=(const TypeName&);
+
+}
+
+#endif
