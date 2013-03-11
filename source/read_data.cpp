679054a827f1f096e2df702d50fededbea23d66b
Ilya Shpigor <petrsum@gmail.com>
2013-03-11 17:10:18 +0400

    Implement the `read-data` utility

---
+++ source/read_data.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <libserial/serial_connection.h>
+
+#include "program_options.h"
+#include "data_parser.h"
+
+using namespace std;
+using namespace serial;
+using namespace serial_utils;
+
+void PrintUsage()
+{
+    cout << "Usage: read-data [options]" << endl;
+    cout << "Options:" << endl;
+    cout << "\t-f FILE\t\tDevice file of the serial port" << endl;
+    cout << "\t-b BAUDRATE\tBaud rate value" << endl;
+    cout << "\t-s SIZE\t\tSize of reading data" << endl;
+    cout << "\t-h\t\tPrint option help" << endl << endl;
+    cout << "Example to read three bytes:" << endl;
+    cout << "\tread-data -f /dev/ttyS0 -b 57600 -s 3" << endl << endl;
+
+    exit(0);
+}
+
+
+int main(int argc, char *argv[])
+{
+    ProgramOptions options(argv, argv+argc);
+
+    if ( options.IsOptionExist("-h") )
+        PrintUsage();
+
+    string dev_file = "";
+    if ( options.IsOptionExist("-f") )
+        dev_file = options.GetString("-f");
+    else
+        PrintUsage();
+
+    int baud_rate;
+    if ( options.IsOptionExist("-b") )
+        baud_rate = options.GetInt("-b");
+    else
+        PrintUsage();
+
+    int size;
+    if ( options.IsOptionExist("-s") )
+        size =  options.GetInt("-s");
+    else
+        PrintUsage();
+
+    SerialConnection connection(dev_file, baud_rate);
+
+    connection.ReceiveAnswer(size);
+
+    return 0;
+}
